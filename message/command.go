b580a0ba91d2543e63112e7d963a5d7937d98b4d
donghquinn <ehdgus1524@gmail.com>
2026-01-11 21:30:52 +0900

    obs publish command

--- message/command.go
+++ message/command.go
@@ -9,16 +9,28 @@ import (
 type Command struct {
 	Name          string
 	TransactionID uint64
+	Args          []any
 }
 
+// publish args
 func DecodeCommand(payload []byte) (*Command, error) {
 	r := bytes.NewReader(payload)
 
 	name, _ := amf.Decode(r)
 	tx, _ := amf.Decode(r)
 
+	var args []any
+	for r.Len() > 0 {
+		v, err := amf.Decode(r)
+		if err != nil {
+			break
+		}
+		args = append(args, v)
+	}
+
 	return &Command{
 		Name:          name.(string),
 		TransactionID: tx.(uint64),
+		Args:          args,
 	}, nil
 }
