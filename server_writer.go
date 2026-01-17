1461dcc8177be6834f2d62e1dda633fe85d41845
donghquinn <ehdgus1524@gmail.com>
2026-01-18 00:10:51 +0900

    error return

--- server_writer.go
+++ server_writer.go
@@ -12,32 +12,32 @@ func writeConnectSuccess(conn net.Conn, tx uint64) error {
 
 	// _result
 	if _, err := amf.Write([]byte{0x02, 0x00, 0x07}); err != nil {
-		return fmt.Errorf("write amf _result err: %v", err)
+		return fmt.Errorf("write amf result err: %v", err)
 	}
 
 	if _, err := amf.WriteString("_result"); err != nil {
-		return fmt.Errorf("write amf _result string err: %v", err)
+		return fmt.Errorf("write amf result string err: %v", err)
 	}
 
 	// transaction id
 	if err := amf.WriteByte(0x00); err != nil {
-		return fmt.Errorf("write transaction id err: %v", err)
+		return fmt.Errorf("write amf transaction id err: %v", err)
 	}
 
 	if err := binary.Write(&amf, binary.BigEndian, float64(tx)); err != nil {
-		return fmt.Errorf("write transaction data err: %v", err)
+		return fmt.Errorf("write binary data err: %v", err)
 	}
 
 	// null
 	if _, err := amf.Write([]byte{0x05}); err != nil {
-		return fmt.Errorf("write null bytes amf err: %v", err)
+		return fmt.Errorf("write amf null bytes err: %v", err)
 	}
 
 	payload := amf.Bytes()
 
 	// chunk (fmt0, csid=3)
 	if _, err := conn.Write([]byte{0x03}); err != nil {
-		return fmt.Errorf("write chunk data err: %v", err)
+		return fmt.Errorf("write chunk err: %v", err)
 	}
 
 	if _, err := conn.Write([]byte{
@@ -50,7 +50,7 @@ func writeConnectSuccess(conn net.Conn, tx uint64) error {
 	}
 
 	if _, err := conn.Write(payload); err != nil {
-		return fmt.Errorf("write amf payload err: %v", err)
+		return fmt.Errorf("write chunk payload err: %v", err)
 	}
 
 	return nil
@@ -61,10 +61,10 @@ func writePublishStart(conn net.Conn, streamID uint32) error {
 
 	// onStatus
 	if _, err := amf.Write([]byte{0x02, 0x00, 0x08}); err != nil {
-		return fmt.Errorf("write on status data err: %v", err)
+		return fmt.Errorf("write amf on status data err: %v", err)
 	}
 	if _, err := amf.WriteString("onStatus"); err != nil {
-		return fmt.Errorf("write on status string err: %v", err)
+		return fmt.Errorf("write amf on status string err: %v", err)
 	}
 
 	// transaction id = 0
@@ -73,7 +73,7 @@ func writePublishStart(conn net.Conn, streamID uint32) error {
 	}
 
 	if err := binary.Write(&amf, binary.BigEndian, float64(0)); err != nil {
-		return fmt.Errorf("write amf binary data err: %v", err)
+		return fmt.Errorf("write binary data err: %v", err)
 	}
 
 	// null
@@ -90,7 +90,7 @@ func writePublishStart(conn net.Conn, streamID uint32) error {
 		return fmt.Errorf("write amf object data err: %v", err)
 	}
 	if _, err := amf.WriteString("NetStream.Publish.Start"); err != nil {
-		return fmt.Errorf("write net stream publish startstring err: %v", err)
+		return fmt.Errorf("write amf net stream publish startstring err: %v", err)
 	}
 
 	if _, err := amf.Write([]byte{
@@ -111,7 +111,7 @@ func writePublishStart(conn net.Conn, streamID uint32) error {
 	payload := amf.Bytes()
 
 	if _, err := conn.Write([]byte{0x03}); err != nil {
-		return fmt.Errorf("write data err: %v", err)
+		return fmt.Errorf("write connection data err: %v", err)
 	}
 
 	if _, err := conn.Write([]byte{
@@ -120,7 +120,7 @@ func writePublishStart(conn net.Conn, streamID uint32) error {
 		0x14,
 		byte(streamID), 0x00, 0x00, 0x00,
 	}); err != nil {
-		return fmt.Errorf("write payload data with stream id err: %v", err)
+		return fmt.Errorf("write connection payload data with stream id err: %v", err)
 	}
 	if _, err := conn.Write(payload); err != nil {
 		return fmt.Errorf("write connection payload err: %v", err)
@@ -129,32 +129,57 @@ func writePublishStart(conn net.Conn, streamID uint32) error {
 	return nil
 }
 
-func writeCreateStreamResult(conn net.Conn, tx uint64, streamID uint32) {
+func writeCreateStreamResult(conn net.Conn, tx uint64, streamID uint32) error {
 	var amf bytes.Buffer
 
 	// _result
-	amf.Write([]byte{0x02, 0x00, 0x07})
-	amf.WriteString("_result")
+	if _, err := amf.Write([]byte{0x02, 0x00, 0x07}); err != nil {
+		return fmt.Errorf("write amf data err: %v", err)
+	}
+	if _, err := amf.WriteString("_result"); err != nil {
+		return fmt.Errorf("write amf result string err: %v", err)
+	}
 
 	// transaction id
-	amf.WriteByte(0x00)
-	binary.Write(&amf, binary.BigEndian, float64(tx))
+	if err := amf.WriteByte(0x00); err != nil {
+		return fmt.Errorf("write amf byte err: %v", err)
+	}
+	if err := binary.Write(&amf, binary.BigEndian, float64(tx)); err != nil {
+		return fmt.Errorf("write binary data err: %v", err)
+	}
 
 	// null
-	amf.Write([]byte{0x05})
+	if _, err := amf.Write([]byte{0x05}); err != nil {
+		return fmt.Errorf("write amf null data err: %v", err)
+	}
 
 	// stream id
-	amf.WriteByte(0x00)
-	binary.Write(&amf, binary.BigEndian, float64(streamID))
+	if err := amf.WriteByte(0x00); err != nil {
+		return fmt.Errorf("write stream id byte err: %v", err)
+	}
+
+	if err := binary.Write(&amf, binary.BigEndian, float64(streamID)); err != nil {
+		return fmt.Errorf("write binary err: %v", err)
+	}
 
 	payload := amf.Bytes()
 
-	conn.Write([]byte{0x03})
-	conn.Write([]byte{
+	if _, err := conn.Write([]byte{0x03}); err != nil {
+		return fmt.Errorf("write connection data err: %v", err)
+	}
+
+	if _, err := conn.Write([]byte{
 		0x00, 0x00, 0x00,
 		byte(len(payload) >> 16), byte(len(payload) >> 8), byte(len(payload)),
 		0x14,
 		0x00, 0x00, 0x00, 0x00,
-	})
-	conn.Write(payload)
+	}); err != nil {
+		return fmt.Errorf("write connection payload data err: %v", err)
+	}
+
+	if _, err := conn.Write(payload); err != nil {
+		return fmt.Errorf("write connection payload err: %v", err)
+	}
+
+	return nil
 }
